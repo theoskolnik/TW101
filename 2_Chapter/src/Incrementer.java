12b3969509338c50e1cbc8ae0ff776841645567c
theoskolnik <theodoraskolnik@gmail.com>
2014-12-09 13:05:38 -0600

    adds chapter 2

---
+++ 2_Chapter/src/Incrementer.java
@@ -0,0 +1,12 @@
+/**
+ * Created by theodoraskolnik on 12/9/14.
+ */
+
+//Create a class that counts how many times you call the method increment()
+// and prints out that number when you call total().
+
+public class Incrementer {
+
+
+
+}
---
+++ 2_Chapter/src/IncrementerTestDrive.java
@@ -0,0 +1,16 @@
+/**
+ * Created by theodoraskolnik on 12/9/14.
+ */
+
+// Create a main method that calls increment 5 times and then calls total.
+
+public class IncrementerTestDrive {
+    public static void main(String[] args) {
+
+        Incrementer i = new Incrementer();
+
+
+
+    }
+
+}
