10381952dd1e3afb7a88b2b44c139a12d472c9f1
theoskolnik <theodoraskolnik@gmail.com>
2014-12-30 13:03:38 -0600

    pseudocode for iso

---
+++ Part1_Exercises/src/IsoscelesTriangle.java
@@ -0,0 +1,25 @@
+/**
+ * Created by theodoraskolnik on 12/30/14.
+ */
+public class IsoscelesTriangle {
+
+    public void numLines(int n) {
+//    for the number of lines
+//    print the spaces and the asterisk
+//    repeat until reach number of lines
+    }
+
+    public void spaces(int n) {
+
+//    find num spaces for first star
+
+        int spaces = n / 2;
+        System.out.print(spaces);
+
+    }
+
+    public static void main(String[] args) {
+        IsoscelesTriangle i = new IsoscelesTriangle();
+        i.numLines(7);
+    }
+}
