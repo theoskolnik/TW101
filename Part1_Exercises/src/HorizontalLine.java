668f2c602090cfaa22063f68988d758d632f1d2c
theoskolnik <theodoraskolnik@gmail.com>
2014-12-30 12:06:12 -0600

    completes horizontal line

--- Part1_Exercises/src/HorizontalLine.java
+++ Part1_Exercises/src/HorizontalLine.java
@@ -2,7 +2,6 @@
  * Created by theodoraskolnik on 12/30/14.
  */
 public class HorizontalLine {
-
     public void numAsterisks(int n) {
         while(n > 0){
             System.out.print("*");
@@ -11,6 +10,7 @@ public class HorizontalLine {
     }
 
     public static void main(String[] args) {
-
+        HorizontalLine h = new HorizontalLine();
+        h.numAsterisks(8);
     }
 }
