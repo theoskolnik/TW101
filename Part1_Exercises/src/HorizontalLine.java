4837010455af397bcc98ab8f1489267f2a278eb2
theoskolnik <theodoraskolnik@gmail.com>
2014-12-30 12:03:08 -0600

    completes one asterisk

---
+++ Part1_Exercises/src/HorizontalLine.java
@@ -0,0 +1,16 @@
+/**
+ * Created by theodoraskolnik on 12/30/14.
+ */
+public class HorizontalLine {
+
+    public void numAsterisks(int n) {
+        while(n > 0){
+            System.out.print("*");
+            n--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Part1_Exercises/src/OneAsterisk.java
@@ -0,0 +1,9 @@
+/**
+ * Created by theodoraskolnik on 12/30/14.
+ */
+public class OneAsterisk {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
+
